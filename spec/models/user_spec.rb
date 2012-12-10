c3dc2fd3382446c49a8ee93778ae0689b1a80549
Paul Bonneau <pbonneau@groupon.com>
2012-12-10 11:58:29 -0800

    Listing 6.31 - 6.32

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,7 +53,7 @@ describe User do
     end
 
     describe "when email format is valid" do
-      it "should bne valid" do
+      it "should be valid" do
         addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.com]
         addresses.each do |valid_address|
           @user.email = valid_address
@@ -70,6 +70,16 @@ describe User do
       end
 
       it { should_not be_valid }
+    end
+
+    describe "email address with mixed case" do
+      let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+      it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        @user.reload.email.should == mixed_case_email.downcase
+      end
     end
 
     describe "when password is not present" do
