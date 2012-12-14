25e8c8e67a2a6317f6e83592486af31c54c12fb5
Paul Bonneau <pbonneau@groupon.com>
2012-12-14 17:33:50 -0600

    Listings 9.29 - 9.35

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name                    "Paul Bonneau"
-    email                   "pbonneau@groupon.com"
-    password                "foobar"
-    password_confirmation   "foobar"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person-#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
