a7c9526f83d3c8984fd3b02561f14673ba7ec31d
Paul Bonneau <pbonneau@groupon.com>
2012-12-19 09:17:46 -0500

    Ch. 9, Ex. 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,9 +51,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    user = User.find(params[:id])
+    if (!user.admin?)
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    end
   end
 
   private
