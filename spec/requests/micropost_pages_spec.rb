1d4e0cd9a94574403817e94d88f00602f242b837
Paul Bonneau <pbonneau@groupon.com>
2012-12-21 14:32:23 -0500

    Listings 10.46 - 10.49

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "MicropostPages" do
+
   subject { page }
 
   let(:user) { FactoryGirl.create(:user) }
@@ -22,9 +23,22 @@ describe "MicropostPages" do
     end
 
     describe "with valid information" do
+      
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
+    end
+  end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end
     end
   end
