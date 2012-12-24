9a6d2f3d6987450e99b520de6c94d300267abf33
Paul Bonneau <pbonneau@groupon.com>
2012-12-24 15:53:21 -0800

    Listings 11.7 - 11.16

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships,
+    foreign_key: "followed_id",
+    class_name: "Relationship",
+    dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
@@ -36,6 +42,17 @@ class User < ActiveRecord::Base
     @user_id
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
